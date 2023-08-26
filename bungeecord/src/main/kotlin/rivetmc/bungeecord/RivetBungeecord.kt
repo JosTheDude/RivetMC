86215e375d495ee9c1b251b64b1628a047b927a0
Jos (BM) <barnav.mitra@icloud.com>
2023-08-25 21:21:51 -0400

    feat: change java to kotlin

--- bungeecord/src/main/kotlin/rivetmc/bungeecord/RivetBungeecord.kt
+++ bungeecord/src/main/kotlin/rivetmc/bungeecord/RivetBungeecord.kt
@@ -1,4 +1,25 @@
-package rivetmc.bungeecord;
+package rivetmc.bungeecord
 
-public class RivetBungeecord {
+import net.md_5.bungee.api.plugin.Plugin
+import rivetmc.common.RivetProxy
+import rivetmc.common.RivetSource
+
+class RivetBungeecord(): Plugin() {
+
+    private val rivet = RivetProxy()
+
+    override fun onEnable() {
+        try {
+            rivet.connect(RivetSource.PRODUCTION, "")
+            rivet.ready()
+        } catch (e: Exception) {
+            logger.severe("Failed to connect to Rivet; is your config.yml set up correctly?")
+            e.printStackTrace()
+        }
+
+
+    }
+
+
+
 }
